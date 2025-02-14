87ab7a424f93b1359f5a6ddf83720d4e0e4488c8
Nicholas Amancio <nick.amancio44@gmail.com>
2025-02-13 22:44:00 -0800

    Trying to fetch all documents from characters collection. Not working due to whitelist...

--- Backend/DatabaseConnection.js
+++ Backend/DatabaseConnection.js
@@ -1,20 +1,18 @@
 import dotenv from 'dotenv';
-
 import mongoose from 'mongoose';
 
 // Uses path of first file to import (should be server.js)
 dotenv.config({path: '.env'});
-
 const URI = process.env.MONGODB_URI
-console.log(URI)
-// mongoose.Promise = global.Promise
 
-// try {
-//     await mongoose.connect(DB_URI);
-// }
-// catch (error) {
-//     console.log("An error occured while connecting to the database: ");
-//     console.log(error);
-// }
+const DatabaseConnection = async() => {
+    try {
+        await mongoose.connect(URI, { serverSelectionTimeoutMS: 5000 })
+        console.log("db connected, yippy !")
+    }
+    catch (error) {
+        console.log(`An error occured while connecting to the database: ${error}`);
+    }
+}
 
-export default mongoose;
+export default DatabaseConnection;
--- Backend/server.js
+++ Backend/server.js
@@ -1,2 +1,23 @@
-import mongoose from './DatabaseConnection.js'
-console.log("hji")
+import DatabaseConnection from './DatabaseConnection.js'
+import character from './schemas/character.js'
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+await DatabaseConnection()
+
+app.get("/getCharacters", async(req, res) => {
+    try {
+        const characters = await character.find()
+        res.json(characters)
+    }
+    catch (e) {
+        console.error("error retrieving Characters: ", e)
+        res.status(500).json({"message": "Error retrieving Characters"})
+    }
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+})
