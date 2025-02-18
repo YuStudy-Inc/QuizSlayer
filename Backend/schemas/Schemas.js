4a5c3c6588d554e41791312d9e4b4cd84c85a9ec
jhhocs <joshuaho1@cpp.edu>
2025-02-17 23:33:29 -0800

    Added Quiz and Question to Schemas.js

--- Backend/schemas/Schemas.js
+++ Backend/schemas/Schemas.js
@@ -1,12 +1,16 @@
 import Character from './Character.js'
 import Enemy from './Enemy.js'
 import Item from './Item.js'
+import Question from './Question.js'
+import Quiz from './Quiz.js'
 import User from './User.js'
 
 export {
     Character,
     Enemy,
     Item,
+    Question,
+    Quiz,
     User
 
 }
