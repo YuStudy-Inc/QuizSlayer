9f7782a4062a4a85daf06cfd638b2fc9a7e787d5
Nicholas Amancio <nick.amancio44@gmail.com>
2025-03-09 16:37:09 -0700

    made the carousel functional ? but it doesn't have any animation. PLEASE HELP

--- Frontend/src/Components/Developer.jsx
+++ Frontend/src/Components/Developer.jsx
@@ -1,13 +1,18 @@
+import { Link } from "react-router-dom"
 import "../Styles/Components/Developer.css"
 
-const Developer = ({ imageOfThem, name, description }) => {
+const Developer = ({ imageOfThem, name, description, LinkedIn, GitHub, active }) => {
     return (
         <>
-            <div className="developer-container">
+            <div className={`developer-container ${active ? "is-active" : ""}`}>
                 <img src={imageOfThem} alt="" />
                 <div className="wise-words-from-this-dev">
                     <h1>{name}</h1>
                     <p>{description}</p>
+                    <div className="dev-links">
+                        <a href={LinkedIn}>&gt;&gt;&gt; LinkedIn &lt;&lt;&lt;</a>
+                        <a href={GitHub}>&gt;&gt;&gt; GitHub &lt;&lt;&lt;</a>
+                    </div>
                 </div>
             </div>
         </>
