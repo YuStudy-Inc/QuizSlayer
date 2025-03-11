d5a0510675320092687c10bf06e27b4fec5c929e
Nicholas Amancio <nick.amancio44@gmail.com>
2025-03-11 00:50:54 -0700

    finished results page. Still need functionality. Need to figure out how to work with the progress bar. Apparently 100% of the width goes outside the bar. :/

--- Frontend/src/Components/ResultCard.jsx
+++ Frontend/src/Components/ResultCard.jsx
@@ -5,22 +5,22 @@ const ResultCard = ({ didWin, coins, monsters, xp, lvl }) => {
         <>
             <div className="results-card-container">
                 <div className="results-stuff">
-                    <h1>Results</h1>
+                    <h1 className="title-in-results-card">Results</h1>
                     <div className="analytics">
-                        <p>Coins {didWin ? "received" : "lost"}</p>
-                        <p>{coins}</p>
-                        <p>Monsters Slain</p>
-                        <p>{monsters}</p>
-                        <p>XP Received</p>
-                        <p>{xp}</p>
+                        <p className="left-results">Coins {didWin ? "received" : "lost"}</p>
+                        <p className="right-results">{coins}</p>
+                        <p className="left-results">Monsters Slain</p>
+                        <p className="right-results">{monsters}</p>
+                        <p className="left-results">XP Received</p>
+                        <p className="right-results">{xp}</p>
                     </div>
                     <div className="progress-bar">
                         <div className="full-bar">
                             <div className="actual-bar"></div>
                         </div>
-                    </div>
-                    <div className="level">
-                        <h1>{lvl}</h1>
+                        <div className="level">
+                            <h1>lv. {lvl}</h1>
+                        </div>
                     </div>
                 </div>
             </div>
--- Frontend/src/Pages/Results.jsx
+++ Frontend/src/Pages/Results.jsx
@@ -10,13 +10,17 @@ const Results = ({didWin, }) => {
     return(
         <>
             <div className="results-container">
+                <div className="results-title">
+                    <h1>{didWin ? win : lose}</h1>
+                </div>
                 <div className="show-results">
-                    <div className="results-title">
-                        <h1>{didWin ? win : lose}</h1>
-                    </div>
                     <div className="container-for-cards-and-character">
-                        <Podium />
-                        <ResultCard didWin={true} coins={"27"} monsters={"3"} xp={"87"} lvl={"9"}/>
+                        <div className="container-for-podium-results-page">
+                            <Podium />
+                        </div>
+                        <div className="container-for-result-card">
+                            <ResultCard didWin={true} coins={"27"} monsters={"3"} xp={"87"} lvl={"9"}/>
+                        </div>
                     </div>
                 </div>
                 <button className="from-results-to-home">Continue</button>
