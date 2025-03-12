b74d347b5bc5e4ee354647bf4d8c90e4d710cf17
jhhocs <joshuaho1@cpp.edu>
2025-03-11 23:02:25 -0700

    Added password toggle

--- Frontend/src/Components/Login/Input/Password.jsx
+++ Frontend/src/Components/Login/Input/Password.jsx
@@ -1,11 +1,35 @@
-import lock_icon from "../../../assets/Login/lock_icon.png"
+import { useState } from "react";
+
+import lockIcon from "../../../assets/Login/lock_icon.png"
+
+import { Icon, SvgIcon } from '@mui/material';
+import eye from '@mui/icons-material/VisibilityOutlined';
+import eyeOff from '@mui/icons-material/VisibilityOffOutlined';
+
 import "./Input.css"
 
 const Password = () => {
+    const[password, setPassword] = useState("");
+    const [icon, setIcon] = useState(eyeOff);
+    const [type, setType] = useState("password");
+
+    const togglePasswordDisplay = () => {
+        if (icon===eyeOff){
+           setIcon(eye);
+           setType("text");
+        } else {
+           setIcon(eyeOff);
+           setType("password");
+        }
+     }
+
     return (
         <div className="input_container">
-            <img src={lock_icon} alt="lock icon" className="icon"></img>
-            <input className="input_box" type="text"></input>
+            <img src={lockIcon} alt="lock icon" className="icon"></img>
+            <input className="input_box" type={type} value={password} onChange={(e) => setPassword(e.target.value)}></input>
+            <span className="password_toggle" onClick={togglePasswordDisplay}>
+                <SvgIcon component={icon}></SvgIcon>
+            </span>
         </div>
     );
 };
