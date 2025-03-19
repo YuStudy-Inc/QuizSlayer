acd2fb2da3c92d2a2d5fdebe4a73228ed815fce4
Nicholas Amancio <112116021+Nickthecan@users.noreply.github.com>
2025-03-18 22:23:49 -0700

    finished the friend's page. Need a overlay to add a new friend and maybe check for new friend requests ??? Maybe Ill do that in the home page

--- Frontend/src/Components/FriendCard.jsx
+++ Frontend/src/Components/FriendCard.jsx
@@ -1,9 +1,20 @@
 import "../Styles/Components/FriendCard.css"
 
-const FriendCard = () => {
+const FriendCard = ({ friendPfp, friendName, friendLevel  }) => {
     return (
         <>
-        
+            <div className="one-friend">
+                <div className="profilepic">
+                    <img src={friendPfp} alt="" />
+                </div>
+                <div className="name">
+                    <h1>{friendName}</h1>
+                </div>
+                <div className="level">
+                    <h1>Lv {friendLevel}</h1>
+                </div>
+                
+            </div>
         </>
     )
 }
--- Frontend/src/Pages/MainMenu/Friends.jsx
+++ Frontend/src/Pages/MainMenu/Friends.jsx
@@ -1,5 +1,8 @@
 import "../../Styles/Pages/Friends.css"
-import FriendCard from "../../Components/Components.js"
+import FriendCard from "../../Components/FriendCard"
+import maomao from "../../assets/Friends/maomao.jpg"
+import Podium from "../../Components/Podium.jsx"
+import addUser from "../../assets/Friends/add-user.png"
 
 const Friends = () => {
     return (
@@ -11,11 +14,34 @@ const Friends = () => {
                             <h1>Friends</h1>
                         </div>
                         <div className="scroll-for-more-friends">
-                            {}
+                            <FriendCard friendPfp={maomao} friendName="nick" friendLevel="10" />
+                            <FriendCard friendPfp={maomao} friendName="nick" friendLevel="10" />
+                            <FriendCard friendPfp={maomao} friendName="nick" friendLevel="10" />
+                            <FriendCard friendPfp={maomao} friendName="nick" friendLevel="10" />
+                            <FriendCard friendPfp={maomao} friendName="nick" friendLevel="10" />
+                            <FriendCard friendPfp={maomao} friendName="nick" friendLevel="10" />
+                            <FriendCard friendPfp={maomao} friendName="nick" friendLevel="10" />
+                            <FriendCard friendPfp={maomao} friendName="nick" friendLevel="10" />
+                            <FriendCard friendPfp={maomao} friendName="nick" friendLevel="10" />
+                            <FriendCard friendPfp={maomao} friendName="nick" friendLevel="10" />
                         </div>
                     </div>
                     <div className="right-side-amigos">
-
+                        <div className="podium-plus-info">
+                            <div className="podi">
+                                <Podium />
+                            </div>
+                            <div className="info">
+                                <h1>Nick{/* {getUsernameOfActiveFriend} */}</h1>
+                                <p>Description :{/*  {getDescriptionOfActiveFriend} */}</p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div className="add-a-friend">
+                        <button className="add-a-friend-button">
+                            <img src={addUser} alt="" />
+                        </button>
                     </div>
                 </div>
             </div>
