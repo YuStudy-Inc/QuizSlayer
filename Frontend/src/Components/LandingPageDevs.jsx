03c5707bc68cd79870e712bde3156d14f495b5a7
jhhocs <joshuaho1@cpp.edu>
2025-03-26 23:47:05 -0700

    feat: Added carousel for Dev section on landing page

--- Frontend/src/Components/LandingPageDevs.jsx
+++ Frontend/src/Components/LandingPageDevs.jsx
@@ -32,14 +32,15 @@ const LandingPageDevs = () => {
                         <h1> Meet The Developers</h1>
                         <div className="underline"></div>
                     </div>
-                    <div className="carousel">
+                    <div className="landing-page-carousel-container">
                         <button onClick={handleClickLeft}><h1>&lt;</h1></button>
-                        {/* I need a sliding animation but idk how to do that. I need it to slide to the left when clicked left and vice versa */}
-                        <div className="developer-box">
-                            <Developer imageOfThem={duckCharacter} name={"Nick Amancio"} description={"hello huzz"} LinkedIn={"https://www.linkedin.com/in/nicholas-amancio/"} GitHub={"https://github.com/Nickthecan"} active={whoActive === 0}/>
-                            <Developer imageOfThem={capyBaraCharacter} name={"Joshua Ho"} description={"hello bruzz"} LinkedIn={"https://www.linkedin.com/in/joshua-h-ho/"} GitHub={"https://github.com/jhhocs"} active={whoActive === 1}/>
-                            <Developer imageOfThem={frogCharacter} name={"Thomas Tejedor"} description={"nah🥀she's🥀got🥀u🥀blushing🥀twin🥀ah🥀hell🥀nah🥀twin🥀u🥀gotta🥀lock🥀up🥀twin🥀bruh🥀this🥀not🥀even🥀u🥀twin🥀on🥀fonem🥀grave🥀bru🥀𝓮𝓾𝓪𝓪𝓱𝓱.😇"} LinkedIn={"https://www.linkedin.com/in/thomas-tejedor-576b952a7/"} GitHub={"https://github.com/ThomasTejedor"} active={whoActive === 2}/>
-                            <Developer imageOfThem={raccoonCharacter} name={"Alvan Zhuang"} description={"hello huzz"} LinkedIn={"https://www.linkedin.com/in/alvanzhuang/"} GitHub={"https://github.com/AlvanZ"} active={whoActive === 3}/>
+                        <div className="carousel">
+                            <div className="developer-box" style={{ '--developer': whoActive}}>
+                                <Developer imageOfThem={duckCharacter} name={"Nick Amancio"} description={"hello huzz"} LinkedIn={"https://www.linkedin.com/in/nicholas-amancio/"} GitHub={"https://github.com/Nickthecan"}/>
+                                <Developer imageOfThem={capyBaraCharacter} name={"Joshua Ho"} description={"hello bruzz"} LinkedIn={"https://www.linkedin.com/in/joshua-h-ho/"} GitHub={"https://github.com/jhhocs"}/>
+                                <Developer imageOfThem={frogCharacter} name={"Thomas Tejedor"} description={"nah🥀she's🥀got🥀u🥀blushing🥀twin🥀ah🥀hell🥀nah🥀twin🥀u🥀gotta🥀lock🥀up🥀twin🥀bruh🥀this🥀not🥀even🥀u🥀twin🥀on🥀fonem🥀grave🥀bru🥀𝓮𝓾𝓪𝓪𝓱𝓱.😇"} LinkedIn={"https://www.linkedin.com/in/thomas-tejedor-576b952a7/"} GitHub={"https://github.com/ThomasTejedor"}/>
+                                <Developer imageOfThem={raccoonCharacter} name={"Alvan Zhuang"} description={"hello huzz"} LinkedIn={"https://www.linkedin.com/in/alvanzhuang/"} GitHub={"https://github.com/AlvanZ"}/>
+                            </div>
                         </div>
                         <button onClick={handleClickRight}><h1>&gt;</h1></button>
                     </div>
