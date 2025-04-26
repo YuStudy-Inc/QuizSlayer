0a3fae0162f0c394fc09a57c47f83f98fec1ab43
jhhocs <joshuaho1@cpp.edu>
2025-04-25 23:40:47 -0700

    feat: Added enums for assets

---
+++ Frontend/src/assets/Hats/HatsEnum.js
@@ -0,0 +1,12 @@
+import { bananaPeel, bucketHat, diamondHelmet, nightCap, pot, wizardHat } from "../Pictures";
+
+const HatsEnum = Object.freeze({
+    1: bucketHat,
+    2: diamondHelmet,
+    3: bananaPeel,
+    4: pot,
+    5: wizardHat,
+    6: nightCap,
+});
+
+export default HatsEnum;
---
+++ Frontend/src/assets/Weapons/WeaponsEnum.js
@@ -0,0 +1,11 @@
+import { fryingPan, ironSword, playfulCloud, redBalloon, someonesGun } from "../Pictures";
+
+const WeaponsEnum = Object.freeze({
+    7: ironSword,
+    8: fryingPan,
+    9: redBalloon,
+    10: someonesGun,
+    11: playfulCloud,
+});
+
+export default WeaponsEnum;
