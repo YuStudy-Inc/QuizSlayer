9fdb936529af0cf08807ead4c69e049c392d9079
Nicholas Amancio <nick.amancio44@gmail.com>
2025-05-03 23:43:41 -0700

    FUCKING FIXED THIS STUPID FUKCING CHANGE I HATE CODING I JUST WANT TO DROP OUT

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 					<Route index element={<LandingPage />}/>
 					<Route path='/' element={<LandingPage />}/>
 					<Route path='/login' element={<LoginSignup />}/>
-					<Route element={<AuthenticateUserRoute/>}>
+					{/* <Route element={<AuthenticateUserRoute/>}> */}
 						<Route path='' element={<Navbar/>}>
 							<Route path='/home' element={<Home />}/>
 							<Route path='/collection' element={<Collection/>}/>
@@ -20,12 +20,12 @@ function App() {
 							<Route path='/leaderboard' element={<Leaderboard />}/>
 						</Route>
 						<Route path='/createquiz' element={<CreateQuizPage />}/>
-						<Route path='/editquiz/:id' element={<EditQuizPage />}/>
+						<Route path='/editquiz/:quizId' element={<EditQuizPage />}/>
 						<Route path='/gacha' element={<Gacha />}/>
 						<Route path='/results' element={<Results />}/>
 						<Route path='/settings' element={<Settings />}/>
 						<Route path="*" element={<PageNotFound />}/>
-					</Route>
+					{/* </Route> */}
 				</Routes>
 			</BrowserRouter>
 		</>
