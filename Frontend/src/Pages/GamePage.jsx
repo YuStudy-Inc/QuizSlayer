70a35f6682dc37e7aa1fb8bc6be053716846e0a1
AlvanZ <alvanzhuang625@gmail.com>
2025-05-04 21:51:21 -0700

    add data for which is used

--- Frontend/src/Pages/GamePage.jsx
+++ Frontend/src/Pages/GamePage.jsx
@@ -1,10 +1,15 @@
 import "../Styles/Pages/GamePage.css";
 import { useEffect, useState } from "react";
 import Alert from "../Components/Alert";
+import UserData from "../UserData";
 const GAME_URI = import.meta.env.VITE_GAME_URI || ''; // Get the game URI from environment variables
 const userId = JSON.parse(localStorage.getItem('id')); // Retrieve user ID from localStorage
-
+const user = JSON.parse(localStorage.getItem('user'));
+console.log(user);
 const GamePage = () => {
+  const character = user.selectedCharacter;
+  const weapon = user.selectedWeapon;
+  const hat = user.selectedHat;
   const [showAlert, setShowAlert] = useState(true)
 	const [alertText, setAlertText] = useState('')
   const [gameFinished, setGameFinished] = useState(false);
@@ -45,7 +50,7 @@ const GamePage = () => {
       <div className="game-page-container">
           {!gameFinished && (
         <iframe
-          src={`${GAME_URI}?sessionId=${userId}`}
+           src={`${GAME_URI}?sessionId=${userId}&character=${character}&weapon=${weapon}&hat=${hat}`}
           style={{ width: "100%", height: "100vh", border: "none" }}
           title="My Game"
         />
